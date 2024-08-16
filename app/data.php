ea438bccab31ba86a916bd1215b1a1aa370becd7
unknown <manjurulus@gmail.comm>
2024-08-16 14:33:24 +0600

    first commit

---
+++ app/data.php
@@ -0,0 +1,99 @@
+<?php
+$contact = [
+    'email' => 'manjurulus@gmail.com',
+    'phone' => '01624 000 000'
+];
+
+$logo = [
+   'url' => 'images/logo01.png'
+];
+
+$socialmedia = [
+    
+    [
+        'link' => 'https://discord.com/',
+        'icon' => 'fa-brands fa-discord'
+    ],
+    [
+        'link' => 'https://www.instagram.com/',
+        'icon' => 'fa-brands fa-instagram'
+    ],
+    [
+        'link' => 'https://www.linkedin.com/',
+        'icon' => 'fa-brands fa-linkedin-in'
+    ],
+    [
+        'link' => 'https://www.facebook.com/',
+        'icon' => 'fa-brands fa-facebook-f'
+    ]
+];
+
+$about = [
+    'title' => 'Our new bangladesh',
+    'desc' => 'The Volkswagen Beetle, officially the Volkswagen Type 1,[a] is a small car produced by the German company Volkswagen from 1938 to 2003.[b] One of the most iconic cars in automotive history, the Beetle is noted for its distinctive shape. Its production period of 65 years is the longest of any single generation of automobile,[c] and its total production of over 21.5 million is the most of any car of a single platform.',
+    'button' => [
+        "title"     => "Apply Now",
+        "link"      => "https://dev-miftaelementor.pantheonsite.io/",
+        "bg_color"  =>  "#00308F"
+    ],
+    'photo' => 'https://picture1.goo-net.com/056/0561562/J/0561562A30231125W00105.jpg'
+
+];
+
+$slider = [
+    [
+        'title' => "হাজী বিরিয়ানি",
+        'desc' => 'হাজী বিরিয়ানি পুরান ঢাকায় অবস্থিত বাংলাদেশের একটি প্রাচীনতম রেস্তোরাঁ যারা খাসীর মাংসের বিরিয়ানি, বোরহানী এবং কোমল পানীয় বিক্রি করে। ১৯৩৯ সালে হাজী মোহাম্মদ হোসেন নামে একজন ব্যক্তি রাস্তার ধারের খাদ্য বিক্রির দোকান হিসেবে রেস্তোরাঁটি চালু করেছিলেন।',
+        'photo' => 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg',
+        'button' => [
+            'btn1' => [
+                'title' => 'Read More',
+                'link' => 'https://dev-miftaelementor.pantheonsite.io/'
+            ],
+            'btn2' => [
+                
+                    'title' => 'Find Us Here',
+                    'link' => 'https://dev-miftaelementor.pantheonsite.io/'
+                ]
+            ]
+            
+    ],
+       
+    
+    [
+        'title' => "কোলকাতা কাচ্চি ঘর",
+        'desc' => 'কোলকাতা কাচ্চি ঘর দেশের সর্বপ্রথম রেস্টুরেন্ট যা বাসমতি চালের কাচ্চি সবাইকে চেখে দেখবার সুযোগ করে দিয়েছিল। যারা একবার এ কাচ্চি খেয়েছেন তারাই জানেন যে ঘ্রাণ, রঙ আর স্বাদ— তিনের কম্বিনেশনে কোলকাতা কাচ্চির কথা ভোলা দায়।',
+        'photo' => 'https://img.delicious.com.au/j95dyjBJ/del/2022/10/australian-capital-territory-kingleys-chicken-176385-3.png',
+        'button' => [
+            'btn1' => [
+                'title' => 'Read More',
+                'link' => 'https://dev-miftaelementor.pantheonsite.io/'
+            ],
+            'btn2' => [
+                
+                    'title' => 'Find Us Here',
+                    'link' => 'https://dev-miftaelementor.pantheonsite.io/'
+                ]
+            ]
+            
+    ],
+    
+    [
+        'title' => "হানিফ বিরিয়ানি",
+        'desc' => 'আলাউদ্দিন রোডে হাজীর বিরিয়ানির ঠিক বিপরীতেই গড়ে উঠেছে পুরান ঢাকার আরেকটি প্রসিদ্ধ বিরিয়ানির দোকান হানিফ বিরিয়ানি। ১৯৭৫ সালে পুরান ঢাকার বাসিন্দা হাজী মোহাম্মদ হানিফের হাত ধরে যাত্রা শুরু করে এটি। ২০০৫ সালে তার মৃত্যুর পর ছেলে হাজী মোহাম্মদ ইব্রাহিম রনি ব্যবসার হাল ধরেন।',
+        'photo' => 'https://images.immediate.co.uk/production/volatile/sites/2/2023/03/Roast-spatchocock-chicken-mojo-rojo-3fe44f9.jpg?quality=90&resize=700,466',
+        'button' => [
+            'btn1' => [
+                'title' => 'Read More',
+                'link' => 'https://dev-miftaelementor.pantheonsite.io/'
+            ],
+            'btn2' => [
+                
+                    'title' => 'Find Us Here',
+                    'link' => 'https://dev-miftaelementor.pantheonsite.io/'
+                ]
+            ]
+            
+        ]
+    ];
+?>
---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+// include functions
+if( file_exists( __DIR__ .'/app/functions.php') ){
+    require_once __DIR__.'/app/functions.php';
+}
+
+// include data.php
+if( file_exists( __DIR__ .'/app/data.php') ){
+    require_once __DIR__.'/app/data.php';
+}
+
+
+?>
